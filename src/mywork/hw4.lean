3777869fd0ea2125abc7c9022ddce48b8e09c850
Michael Fatemi <myfatemi04@gmail.com>
2022-10-16 16:16:53 -0400

    HW4 and review

--- src/mywork/hw4.lean
+++ src/mywork/hw4.lean
@@ -12,9 +12,7 @@ too. Replace the placeholder (_) with your
 answer.
 -/
 
-def and_associative : Prop := 
-  ∀ (P Q R : Prop), P ∧ (Q ∧ R) ↔ (P ∧ Q) ∧ R
-
+def and_associative := ∀ (P Q R), P ∧ (Q ∧ R) ↔ (P ∧ Q) ∧ R
 
 /- #1B [10 points]
 
@@ -24,7 +22,10 @@ that you use in your reasoning.
 -/
 
 /-
-Answer: If P and (Q and R) is true, then P is true, and (Q and R) is
+Answer: Because this is a problem of biimplication, it suffices
+to show that both directions are true independently of each other.
+
+1. If P and (Q and R) is true, then P is true, and (Q and R) is
 true [and elimination]. If (Q and R) is true, then Q is true and R is
 true [and elimination]. Therefore, P, Q, and R are true. Using
 and introduction, we can conclude from P being true and Q being true
@@ -32,8 +33,15 @@ that (P and Q) is true. From (P and Q) being true and R being true,
 we can use and introduction again to conclude that (P and Q) and R is
 true.
 
-If we say that (P and (Q and R) is false) --> ((P and Q) and R is true),
-then 
+2. If (P and Q) and R is true, then (P and Q) is true and R is true
+[and elimination]. If (P and Q) is true, then P is true and Q is true
+[and elimination]. Therefore, P, Q, and R are true. Using and introduction,
+we can conclude from Q being true and R being true that (Q and R) is
+true. From P being true and (Q and R) being true, we can use and
+introduction again to conclude that P and (Q and R) is true.
+
+Because the propositions are true in both directions, they must be
+true iff each other.
 -/
 
 /- #1C [5 points]
@@ -47,11 +55,24 @@ and_assoc_true: and_associative
 :=
 begin
   unfold and_associative,
+  -- Start a for-all introduction
   assume P Q R,
-  let ae1: P := and.elim_left and_associative,
-  let ae2: (Q ∧ R) := and.elim_right and_associative,
-  let ae3: Q := and.elim_left ae2,
-  let ae4: R := and.elim_right ae2,
+  -- Now, we just need to prove a bi-implication. We do this with
+  -- iff.intro. This splits us into two subgoals.
+  apply iff.intro _ _,
+
+  -- Cases is used to decompose an OR or an AND into two cases.
+  assume pqr,
+  cases pqr with p qr,
+  cases qr with q r,
+  -- "let" is used for proofs. "assume" is used for propositions.
+  let pq := and.intro p q,
+  exact and.intro pq r,
+
+  assume pqr,
+  cases pqr with pq r,
+  cases pq with p q,
+  exact and.intro p (and.intro q r),
 end
 
 
@@ -73,6 +94,14 @@ the specific inference rules you use in your
 reasoning.
 -/
 
+/-
+Answer: Because this is a problem of biimplication, it suffices
+to show that both directions are true independently of each other.
+
+1. Assume that P ∨ (Q ∨ R) is true. 
+
+-/
+
 
 /- #2C [5 points]
 
@@ -81,6 +110,36 @@ Complete the following formal proof.
 
 theorem or_associative_true : or_associative :=
 begin
+unfold or_associative,
+assume P Q R,
+
+apply iff.intro _ _,
+
+-- Forward direction
+assume pqr,
+cases pqr with p q_or_r,
+exact or.intro_left R (or.intro_left Q p),
+
+cases q_or_r with q r,
+let pq := (or.intro_right P q),
+exact or.intro_left R pq,
+
+exact or.intro_right (P ∨ Q) r,
+
+assume pqr,
+cases pqr with p_or_q r,
+-- Case for p_or_q
+cases p_or_q with p q,
+-- Case for p
+exact or.intro_left (Q ∨ R) p,
+-- Case for q
+let qr := or.intro_left R q,
+exact or.intro_right P qr,
+
+-- Case for r
+let qr := or.intro_right Q r,
+exact or.intro_right P qr,
+
 end
 
 
@@ -89,13 +148,12 @@ Write a formal statement of the proposition.
 -/
 
 def arrow_transitive : Prop :=
-  _
-
+  ∀ (X Y Z: Prop), (X → Y) → (Y → Z) → (X → Z)
 
 /- #3B [10 points]
 
 Write an English language proof of the proposition
-that for any propositions, X, Y, and X, it's the
+that for any propositions, X, Y, and Z, it's the
 case that (X → Y) → (Y → Z) → (X → Z). In other
 words, implication is "transitive." Hint: Recall
 that if you have a proof of, say, X → Y, and you 
@@ -105,10 +163,30 @@ of an implication to a proof of its premise to get
 yourself a proof of its conclusion.
 -/
 
+/-
+Answer: Because this is a problem of implication, it suffices
+to show that whenever the context is true, the conclusion must
+follow. Therefore, we can start by assuming that X implies Y, that
+Y implies Z, and finally that X is true.
+
+If we assume X, then because X implies Y, we can use arrow elimination
+to conclude that Y is true. If we assume Y, then because Y implies Z,
+we can use arrow elimination to conclude that Z is true. Therefore, Z is
+true whenever X is true, so our proposition is valid.
+-/
+
 
 /- #3C [5 points]. 
 Write a formal proof of it.
 -/
+example: arrow_transitive :=
+begin
+assume X Y Z,
+assume xy yz,
+assume x,
+
+exact yz (xy x)
+end
 
 
 /- #4
@@ -125,18 +203,37 @@ logic by completing the following answer.
 
 def contrapositive : Prop :=
   ∀ (Raining Wet : Prop), 
-    (Raining → Wet) → (¬Raining → ¬Wet)
+    (Raining → Wet) → (¬Wet → ¬Raining)
 
 
-/- #4B [10 points]. 
+/- #4B [10 points].
 -/
 
 theorem contrapositive_valid : contrapositive :=
 begin
+unfold contrapositive,
+assume Raining Wet,
+assume rw,
+assume not_wet,
+
+-- Show that rain is impossible
+assume raining,
+-- False elimination
+exact not_wet (rw raining),
+end
 
 /- #4C [5 points]. 
 
 Give an English language proof of it.
+
+We assume that rain implies a wet street, and we want to prove
+that a dry street implies no rain. Our tactic is to assume that
+the street is dry, and then show that rain is impossible. We can
+do this through contradiction. If the street is dry, but it is also
+raining, then we can use the assumption that rain implies a wet
+street to derive a contradiction. Therefore, we can prove that
+if rain implies a wet street, and we know that the street is not
+wet, then we can conclude that it is not raining.
 -/
 
 
@@ -155,10 +252,19 @@ theorem demorgan1 :
     ∀ (X Y : Prop), 
       ¬(X ∨ Y) → (¬X ∧ ¬Y) :=
 begin
-assume em X Y nxory,
+assume em X Y nx_or_y,
 cases (em X) with x nx,
-let foo := or.intro_left Y x,
-_
+-- Case for X being true
+-- Show that X being true makes ¬(X ∨ Y) false
+let x_or_y := or.intro_left Y x,
+contradiction,
+
+-- Case for X being false
+apply and.intro nx _,
+-- Show that y cannot be true
+assume y,
+let x_or_y := or.intro_right X y,
+contradiction,
 end
 
 /-
--- src/review/22_10_16_review.lean
+++ src/review/22_10_16_review.lean
@@ -1,3 +1,115 @@
-/-
+example: ∀ (P Q R: Prop), P ∨ Q → ((P → R) → (Q → R) → R) :=
+begin
+assume P Q R,
+assume (h: P ∨ Q), -- Allows you to document your reasoning more thoroughly.
+apply or.elim h,
 
--/
+-- First case: assume P
+assume (p: P), -- Arrow introduction
+assume (pr: P → R),
+assume (qr: Q → R),
+exact (pr p),
+
+-- Second case: assume Q
+assume (q: Q),
+assume (pr: P → R),
+assume (qr: Q → R),
+
+exact (qr q),
+end
+
+#check or.inl
+#check or.inr
+
+-- What does the unfold command do?
+
+def x := 5
+
+theorem foo: x + 1 = 6 :=
+begin
+unfold x, -- Unfolds the definition of x (replaces x with 5)
+end
+
+-- Axiom of the excluded middle
+def m := ∀ P, P ∨ ¬P
+
+theorem bar: m :=
+begin
+unfold m, -- Just replaces a name with its meaning
+assume P,
+apply classical.em
+end
+
+-- How do I work with iff?
+-- Iff is called a bi-implication
+-- You can prove this through iff.intro
+
+example: ∀ (P Q: Prop), P ↔ Q :=
+begin
+assume P Q,
+let pq: P → Q := sorry,
+let qp: Q → P := sorry,
+apply iff.intro pq qp,
+end
+
+-- English language proof of a bi-implication
+-- It would suffice to have a proof of P → Q and a proof of Q → P
+-- Let's take the forward direction first (...), then let's
+-- go in the other direction (...)
+
+variables (P Q: Prop)
+variables (pq: P → Q) (qp: Q → P)
+example: P ↔ Q := iff.intro pq qp
+
+-- There are slightly weird things about the names of elimination rules.
+
+variable piffq: P ↔ Q
+
+example: P → Q := iff.mp piffq
+example: Q → P := iff.mpr piffq
+
+theorem my_and_elim: ∀ (P Q), P ∧ Q → P :=
+begin
+-- We want to prove a for-all here. We do this by assuming the
+-- premises are given, and then showing that what follows is a
+-- proof of the conclusion.
+assume P Q,
+
+-- Assume that the premise is true. (If we want to prove an implication,
+-- it suffices to assume the context and prove the conclusion.)
+assume pq: P ∧ Q,
+
+-- Cases for "and" are called "and.elim_left" and "and.elim_right"
+cases pq,
+exact pq_left,
+end
+
+variables (Smoke Fire Light Good: Prop)
+variables (sf: Smoke → Fire) (fl: Fire → Light) (lg: Light → Good)
+variable (s: Smoke)
+
+example: ∀ (S F L G: Prop), (S → F) → (F → L) → (L → G) → S → G :=
+begin
+assume S F L G,
+assume sf fl lg,
+
+-- This is taken from what we're trying to prove (the S → G)
+assume s,
+exact lg (fl (sf s))
+end
+
+-- Some review for negation elimination
+-- It's really helpful to remember the truth table for NOT.
+
+example: ∀ P, ¬(P ∧ ¬P) :=
+begin
+assume P,
+
+-- This is the "mistake" that we're trying to prove doesn't happen.
+-- Because of this, we will be able to construct a proof of false.
+assume (h: P ∧ ¬P),
+
+cases h with p not_p,
+-- Remember: ¬P is the same as P → false
+apply not_p p,
+end
